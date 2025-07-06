2bb1b9d431d832e481cb92206531e78db3be0ae8
Amirreza Rashidi <amirrezarashidi5831ar@gmail.com>
2025-07-07 00:40:28 +0330

    comments added

--- blue-screen/blue-screen.ino
+++ blue-screen/blue-screen.ino
@@ -1,26 +1,26 @@
 #include <Keyboard.h>
 
 void setup() {
-  delay(2000);
+  delay(2000);//2 sec delay
   Keyboard.begin();
 
   // Run (Win + R)
-  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press(KEY_LEFT_GUI);//windows key
   Keyboard.press('r');
   delay(200);
   Keyboard.releaseAll();
   delay(500);
 
   // photo location
-  Keyboard.print("C:\\Users\\Asus\\Desktop\\bluesc\\bluescreen.avif");
+  Keyboard.print("bluesc\\bluescreen.avif");//enter your own adress
   delay(500);
 
-  Keyboard.press(KEY_RETURN);
+  Keyboard.press(KEY_RETURN);//enter key
   Keyboard.release(KEY_RETURN);
   delay(600);  
 
   // F11 
-  Keyboard.press(KEY_F11);
+  Keyboard.press(KEY_F11);//f11 key 
   delay(100);
   Keyboard.release(KEY_F11);
 
