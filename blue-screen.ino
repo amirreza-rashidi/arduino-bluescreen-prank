7b2f39e83db9854fdfe36edcdf6a41cd2c562053
Amirreza Rashidi <amirrezarashidi5831ar@gmail.com>
2025-07-06 14:04:15 +0330

    upload files

---
+++ blue-screen.ino
@@ -0,0 +1,30 @@
+#include <Keyboard.h>
+
+void setup() {
+  delay(2000);
+  Keyboard.begin();
+
+  // Run (Win + R)
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  delay(200);
+  Keyboard.releaseAll();
+  delay(500);
+
+  // photo location
+  Keyboard.print(" enter the location of picture here ");
+  delay(500);
+
+  Keyboard.press(KEY_RETURN);
+  Keyboard.release(KEY_RETURN);
+  delay(600);  
+
+  // F11 
+  Keyboard.press(KEY_F11);
+  delay(100);
+  Keyboard.release(KEY_F11);
+
+  Keyboard.end();
+}
+
+void loop() {}
