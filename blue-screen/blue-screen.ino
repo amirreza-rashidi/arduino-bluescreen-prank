1ab43365913804d08dc22115d928ee2b4af66910
Amirreza Rashidi <amirrezarashidi5831ar@gmail.com>
2025-07-30 21:54:30 +0330

    new

--- blue-screen/blue-screen.ino
+++ blue-screen/blue-screen.ino
@@ -3,23 +3,27 @@
 void setup() {
   delay(2000);//2 sec delay
   Keyboard.begin();
-
-  // Run (Win + R)
+ 
+  
+  //  Run (Win + R)
   Keyboard.press(KEY_LEFT_GUI);//windows key
   Keyboard.press('r');
   delay(200);
   Keyboard.releaseAll();
   delay(500);
 
-  // photo location
+ 
+  //  photo location
   Keyboard.print("bluesc\\bluescreen.avif");//enter your own adress
   delay(500);
 
+  
   Keyboard.press(KEY_RETURN);//enter key
   Keyboard.release(KEY_RETURN);
   delay(600);  
 
-  // F11 
+  //  F11 
+ 
   Keyboard.press(KEY_F11);//f11 key 
   delay(100);
   Keyboard.release(KEY_F11);
